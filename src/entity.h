5c9bdf1f7f3dc69411c0b355869a47a354dc95f3
Albert Vaca Cintora <albertvaka@gmail.com>
2020-03-28 20:08:26 +0100

    Debug draw in entities

--- src/entity.h
+++ src/entity.h
@@ -14,10 +14,30 @@ struct Entity
 
 struct BoxEntity : Entity {
 	vec size = vec(16, 16);
+
+	Bounds bounds() {
+		return Bounds(pos, size, true);
+	}
+
+	void drawBounds(sf::RenderTarget& window, sf::Color color = sf::Color::Red, sf::Color fillColor = sf::Color::Transparent) {
+		bounds().Draw(window, color, fillColor);
+	}
 };
 
 struct CircleEntity : Entity {
 	float radius = 8.f;
+	void drawBounds(sf::RenderTarget& window, sf::Color color = sf::Color::Red, sf::Color fillColor = sf::Color::Transparent) {
+		sf::CircleShape cs(radius);
+		cs.setRadius(radius);
+		cs.setOrigin(vec(radius,radius));
+		cs.setPosition(pos);
+
+		cs.setFillColor(fillColor);
+		cs.setOutlineColor(color);
+		cs.setOutlineThickness(1);
+
+		window.draw(cs);
+	}
 };
 
 
