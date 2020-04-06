57fb8841002ce6e3cb12ac649da639e4a2a56568
Albert Vaca Cintora <albertvaka@gmail.com>
2020-04-06 21:36:27 +0200

    Added note

--- src/selfregister.h
+++ src/selfregister.h
@@ -33,6 +33,11 @@ public:
 	}
 	virtual ~EntS()
 	{
+		// This is quite inefficient becuase it has to walk the entire array to find the element and then compact it.
+		// This could be done in a single pass in deleteNotAlive/deleteAll for all pointers, which already walks the array (and deletes from it, which calls this!), but only if:
+		// (a) we never delete manually anywhere else (or we do so with a function that does call erase for that element like here) and
+		// (b) the same object is never on two different EntS vectors (could happen with inheritance)
+		// If the (b) is true that means we can also make this destructor non-virtual and not force all EntS classes to be polymorphic
 		getAll().erase(std::remove(getAll().begin(), getAll().end(), (T*)this), getAll().end());
 	}
 	static void sort(std::function<bool(T*,T*)> sortBy) {
