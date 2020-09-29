d217cafa92b9620c712756ace355b1dbf983e8c0
Albert Vaca Cintora <albertvaka@gmail.com>
2020-09-30 00:27:10 +0200

    SelfRegister from the future

--- engine/selfregister.h
+++ engine/selfregister.h
@@ -4,6 +4,9 @@
 #include <functional>
 #include <algorithm>
 
+#define SUPPORT_MULTIPLE_SELFREGISTER_INHERITANCE true
+#define ENABLE_OPTIMIZATION_BULK_DELETE_ALL false
+
 /**
  * Inheriting from this class gives you a static method GetAll()
  * that returns a set with all the current instances of the class.
@@ -24,23 +27,30 @@ public:
 	{
 		GetAll().push_back((T*)this);
 	}
+
 	static std::vector<T*>& GetAll()
 	{
 		static std::vector<T*> instancies;
 		return instancies;
 	}
-	virtual ~SelfRegister()
-	{
-		// This is quite inefficient becuase it has to walk the entire array to find the element and then compact it.
-		// This could be done in a single pass in DeleteNotAlive/DeleteAll for all pointers, which already walks the array (and deletes from it, which calls this!), but only if:
-		// (a) we never delete manually anywhere else (or we do so with a function that does call erase for that element like here) and
-		// (b) the same object is never on two different SelfRegister vectors (could happen with inheritance)
-		// If the (b) is true that means we can also make this destructor non-virtual and not force all SelfRegister classes to be polymorphic
-		GetAll().erase(std::remove(GetAll().begin(), GetAll().end(), (T*)this), GetAll().end());
-	}
-	static void Sort(std::function<bool(T*,T*)> sortBy) {
+
+	static void Sort(std::function<bool(T*, T*)> sortBy) {
 		std::sort(GetAll().begin(), GetAll().end(), sortBy);
 	}
+
+#if SUPPORT_MULTIPLE_SELFREGISTER_INHERITANCE
+	// This destructor doesn't need to be virtual if we guarantee the same object is never
+	// on two different SelfRegister vectors (could happen with inheritance)
+	virtual
+#endif
+	~SelfRegister()
+	{
+#if ENABLE_OPTIMIZATION_BULK_DELETE_ALL
+		if (InBulkDelete()) return;
+#endif
+		GetAll().erase(std::remove(GetAll().begin(), GetAll().end(), (T*)this), GetAll().end());
+	}
+
 	static void DeleteNotAlive()
 	{
 		for (int i = GetAll().size()-1; i >= 0; i--)
@@ -52,12 +62,29 @@ public:
 			}
 		}
 	}
+
 	static void DeleteAll()
 	{
+#if ENABLE_OPTIMIZATION_BULK_DELETE_ALL
+		InBulkDelete() = true;
+#endif
 		for (int i = GetAll().size()-1; i >= 0; i--)
 		{
 			T* e = GetAll()[i];
 			delete e;
 		}
+#if ENABLE_OPTIMIZATION_BULK_DELETE_ALL
+		GetAll().clear();
+		InBulkDelete() = false;
+#endif
 	}
+
+#if ENABLE_OPTIMIZATION_BULK_DELETE_ALL
+private:
+	static bool& InBulkDelete()
+	{
+		static bool running = false;
+		return running;
+	}
+#endif
 };
