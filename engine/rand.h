f9f9f1e05daa8dd63df91a5d39b5a25f675cba8c
Albert Vaca Cintora <albertvaka@gmail.com>
2021-01-28 18:42:50 +0100

    Some more nodiscards

--- engine/rand.h
+++ engine/rand.h
@@ -52,7 +52,7 @@ namespace Rand
 
 namespace GoodRand
 {
-	typedef int unsigned uint;
+	typedef unsigned int uint;
 
 	inline struct RndEngine
 	{
@@ -90,16 +90,16 @@ namespace GoodRand
 	} r;
 
 	//Pre-defined distributions are faster than asking for an arbitrary range
-	inline uint roll_flipcoin() { return r.distr_coin(r.gen); } // [0,1]
-	inline uint roll_1d3() { return r.distr_1d3(r.gen); } // [0,2]
-	inline uint roll_1d4() { return r.distr_1d4(r.gen); } // [0,3]
-	inline uint roll_1d6() { return r.distr_1d6(r.gen); } // ...
-	inline uint roll_1d10() { return r.distr_1d10(r.gen); }
-	inline uint roll_1d20() { return r.distr_1d20(r.gen); }
-	inline uint roll_1d100() { return r.distr_1d100(r.gen); }
-	inline uint roll_1d360() { return r.distr_1d360(r.gen); }
+	[[nodiscard]] inline uint roll_flipcoin() { return r.distr_coin(r.gen); } // [0,1]
+	[[nodiscard]] inline uint roll_1d3() { return r.distr_1d3(r.gen); } // [0,2]
+	[[nodiscard]] inline uint roll_1d4() { return r.distr_1d4(r.gen); } // [0,3]
+	[[nodiscard]] inline uint roll_1d6() { return r.distr_1d6(r.gen); } // ...
+	[[nodiscard]] inline uint roll_1d10() { return r.distr_1d10(r.gen); }
+	[[nodiscard]] inline uint roll_1d20() { return r.distr_1d20(r.gen); }
+	[[nodiscard]] inline uint roll_1d100() { return r.distr_1d100(r.gen); }
+	[[nodiscard]] inline uint roll_1d360() { return r.distr_1d360(r.gen); }
 
-	inline float rollf() { return r.uniform(r.gen); } // [0.f,1.f)
+	[[nodiscard]] inline float rollf() { return r.uniform(r.gen); } // [0.f,1.f)
 
 	template<typename _T>
 	void shuffle(_T* values, size_t count)
@@ -115,7 +115,7 @@ namespace GoodRand
 
 	//returns a random number with a normal distribution. See method at
 	//http://www.taygeta.com/random/gaussian.html
-	inline float Gaussian(float mean = 0.0, float standard_deviation = 1.0)
+	[[nodiscard]] inline float Gaussian(float mean = 0.0, float standard_deviation = 1.0)
 	{
 		float x1, x2, w, y1;
 		static float y2;
