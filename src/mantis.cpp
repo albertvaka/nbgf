albertvaka/nbgf

commit 38d93eb57ce87579cb1a1fb84e6ae75415b52f03

Added FlyingAlien enemy

src/mantis.cpp
@@ -50,7 +50,7 @@ bool Mantis::IsBouncingAgainstAnotherMantis() {
 	if (!collidingWith) {
 		return false;
 	}
-	return (collidingWith->pos.x > pos.x && vel.x > 0) || (collidingWith->pos.x < pos.x&& vel.x < 0);
+	return IsMovingTowardsInY(pos, vel, collidingWith->pos);
 }
 
 void Mantis::Update(float dt)

asset_src/level.tmx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<map version="1.4" tiledversion="1.4.2" orientation="orthogonal" renderorder="right-down" width="100" height="100" tilewidth="16" tileheight="16" infinite="1" backgroundcolor="#321f2432" nextlayerid="12" nextobjectid="222">
+<map version="1.4" tiledversion="1.4.2" orientation="orthogonal" renderorder="right-down" width="100" height="100" tilewidth="16" tileheight="16" infinite="1" backgroundcolor="#321f2432" nextlayerid="12" nextobjectid="231">
  <editorsettings>
   <export target="." format="tmx"/>
  </editorsettings>
@@ -16,12 +16,14 @@
   <tile id="26" type="lslope"/>
   <tile id="28" type="oneway"/>
   <tile id="30" type="breakable"/>
+  <tile id="37" type="debug_teleport"/>
   <tile id="39" type="rslope"/>
   <tile id="40" type="lslope"/>
   <tile id="41" type="lslope"/>
   <tile id="42" type="rslope"/>
   <tile id="43" type="goomba"/>
   <tile id="45" type="goombacharger"/>
+  <tile id="47" type="flyingalien"/>
   <tile id="53" type="breakable">
    <properties>
     <property name="alias" value="simple"/>
@@ -266,12 +268,30 @@
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,116,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0
+</chunk>
+   <chunk x="-176" y="-32" width="16" height="16">
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,116,145,145,87,0,0,0,0,0,0,116,145,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+145,145,87,0,0,0,0,0,0,116,145,145,87,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,116,145,145,87,0,0,0,0,0,0,116,145,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 </chunk>
    <chunk x="-160" y="-32" width="16" height="16">
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
@@ -281,13 +301,13 @@
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
+145,87,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
+0,0,0,116,145,145,87,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
+145,87,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0
 </chunk>
@@ -400,58 +420,58 @@
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 </chunk>
    <chunk x="-192" y="-16" width="16" height="16">
+0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,116,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,116,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,79,0,0,0,0,116,145,145,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0
 </chunk>
    <chunk x="-176" y="-16" width="16" height="16">
+145,145,87,0,0,0,0,0,0,116,145,145,87,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,116,145,145,87,0,0,0,0,0,0,116,145,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+145,145,87,0,0,0,0,0,0,116,145,145,87,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,116,145,145,87,0,0,0,0,0,0,116,145,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+145,145,145,145,145,145,145,145,145,145,145,145,145,145,145,145,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,116,145,145,58,0,0,0,0,0,0,116,145
+0,0,0,0,116,145,145,87,0,0,0,0,0,0,116,145
 </chunk>
    <chunk x="-160" y="-16" width="16" height="16">
+0,0,0,116,145,145,87,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
+145,87,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
+0,0,0,116,145,145,87,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
+145,87,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
+145,145,145,145,145,145,145,145,87,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
-145,58,0,0,0,0,0,0,0,0,0,0,0,79,0,0
+145,87,0,0,0,0,0,0,0,0,0,0,0,79,0,0
 </chunk>
    <chunk x="-128" y="-16" width="16" height="16">
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
@@ -681,7 +701,7 @@
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,116,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,79,0,116,145,145,58,0,0,
+0,0,0,0,0,0,0,0,79,0,116,145,145,87,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,79,0,0,0,0,0,0,116,
@@ -690,31 +710,31 @@
    <chunk x="-176" y="0" width="16" height="16">
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-145,145,58,0,0,0,0,0,0,116,145,145,58,0,0,0,
+145,145,87,0,0,0,0,0,0,116,145,145,87,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,116,145,145,58,0,0,0,0,0,0,116,145,
+0,0,0,0,116,145,145,87,0,0,0,0,0,0,116,145,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-145,145,58,0,0,0,0,0,0,116,145,145,58,0,0,0,
+145,145,87,0,0,0,0,0,0,116,145,145,87,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,116,145,145,58,0,0,0,0,0,0,0,0,
+0,0,0,0,116,145,145,87,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,72,72,72,72,72,72,72,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-145,145,58,0,0,0,0,0,0,0,0,0,0,0,0,0,
+145,145,87,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 </chunk>
    <chunk x="-160" y="0" width="16" height="16">
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
-0,0,0,116,145,145,58,0,0,0,0,0,0,79,0,0,
+0,0,0,116,145,145,87,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
-145,58,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
+145,87,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,79,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,116,145,145,58,0,0,0,0,0,0,0,0,0,
+0,0,0,116,145,145,87,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
@@ -1465,10 +1485,15 @@
   <object id="211" gid="13" x="-1526" y="656.333" width="16" height="16"/>
   <object id="212" gid="13" x="-1246.67" y="672.333" width="16" height="16"/>
   <object id="213" gid="354" x="-2031" y="457" width="16" height="16"/>
-  <object id="217" type="angrybat" gid="117" x="-2603" y="-113.667" width="16" height="16"/>
-  <object id="218" type="angrybat" gid="117" x="-2589" y="-99.6667" width="16" height="16"/>
+  <object id="217" type="angrybat" gid="117" x="-2499.91" y="-345.303" width="16" height="16"/>
+  <object id="218" type="angrybat" gid="117" x="-2485.91" y="-331.303" width="16" height="16"/>
   <object id="220" gid="465" x="-165.27" y="175.958" width="16" height="16"/>
   <object id="221" gid="465" x="274.25" y="96.1042" width="16" height="16"/>
+  <object id="222" gid="48" x="-2808.69" y="-134.212" width="16" height="16"/>
+  <object id="223" gid="48" x="-2589.58" y="-132.909" width="16" height="16"/>
+  <object id="224" gid="48" x="-2801.88" y="89.2421" width="16" height="16"/>
+  <object id="225" gid="48" x="-2534.33" y="29.2727" width="16" height="16"/>
+  <object id="230" gid="38" x="-2707" y="-26.6667" width="16" height="16"/>
  </objectgroup>
  <objectgroup id="9" name="Notes" visible="0">
   <object id="112" x="-532.167" y="-16.1667" width="101.667" height="41.6667">

gaem.vcxproj
@@ -58,6 +58,7 @@
     <ClInclude Include="src\enemy_door.h" />
     <ClInclude Include="src\fireshot.h" />
     <ClInclude Include="src\fireslime.h" />
+    <ClInclude Include="src\flyingalien.h" />
     <ClInclude Include="src\goomba.h" />
     <ClInclude Include="src\jumpman.h" />
     <ClInclude Include="src\lava.h" />
@@ -129,6 +130,7 @@
     <ClCompile Include="src\enemy_door.cpp" />
     <ClCompile Include="src\fireshot.cpp" />
     <ClCompile Include="src\fireslime.cpp" />
+    <ClCompile Include="src\flyingalien.cpp" />
     <ClCompile Include="src\fxmanager.cpp" />
     <ClInclude Include="src\fxmanager.h" />
     <ClCompile Include="src\goomba.cpp" />

gaem.vcxproj.filters
@@ -313,6 +313,9 @@
     <ClInclude Include="src\texturepackerexport.h">
       <Filter>src\generated</Filter>
     </ClInclude>
+    <ClInclude Include="src\flyingalien.h">
+      <Filter>src\entities\enemies</Filter>
+    </ClInclude>
   </ItemGroup>
   <ItemGroup>
     <ClCompile Include="vendor\glew\glew.c">
@@ -494,6 +497,9 @@
     </ClCompile>
     <ClCompile Include="src\tiledexport.cpp">
       <Filter>src\generated</Filter>
+    </ClCompile>
+    <ClCompile Include="src\flyingalien.cpp">
+      <Filter>src\entities\enemies</Filter>
     </ClCompile>
   </ItemGroup>
   <ItemGroup>

src/anim_lib.h
@@ -85,6 +85,17 @@ namespace AnimLib {
 		{ TexturePackerRects::EXPLOSION_09, 0.05f },
 	};
 
+	constexpr const AnimationFrame FLYING_ALIEN[] = {
+		{ TexturePackerRects::FLYINGALIEN_01, 0.05f },
+		{ TexturePackerRects::FLYINGALIEN_02, 0.05f },
+		{ TexturePackerRects::FLYINGALIEN_03, 0.05f },
+		{ TexturePackerRects::FLYINGALIEN_04, 0.05f },
+		{ TexturePackerRects::FLYINGALIEN_05, 0.05f },
+		{ TexturePackerRects::FLYINGALIEN_06, 0.05f },
+		{ TexturePackerRects::FLYINGALIEN_07, 0.05f },
+		{ TexturePackerRects::FLYINGALIEN_08, 0.05f },
+	};
+
 	constexpr const GPU_Rect BIPEDAL_IDLE = TexturePackerRects::BIPEDALUNIT_IDLE_00;
 	
 	constexpr const GPU_Rect FIRESHOT = {437, 5, 6, 6};

src/common_tilemapcharacter.h
@@ -35,6 +35,14 @@ inline bool IsGrounded(const vec& pos, const vec& size, const vec& marginPixels 
 	return false;
 }
 
+inline bool IsMovingTowardsInX(const vec& pos, const vec& vel, const vec& target) {
+	return (vel.x < 0 && pos.x > target.x) || (vel.x > 0 && pos.x < target.x);
+}
+
+inline bool IsMovingTowardsInY(const vec& pos, const vec& vel, const vec& target) {
+	return (vel.y < 0 && pos.y > target.y) || (vel.y > 0 && pos.y < target.y);
+}
+
 inline bool IsGoingToRunOffPlatform(const vec& pos, const vec& size, const vec& vel, float dt) {
 	vec newPos = pos + vel * dt;
 	float toTheSideImMoving = vel.x > 0 ? size.x / 2 : -size.x / 2;

src/flyingalien.cpp
@@ -1,0 +1,187 @@
+#include "flyingalien.h"
+
+#include "jumpman.h"
+#include "collide.h"
+#include "window.h"
+#include "assets.h"
+#include "shader.h"
+#include "rand.h"
+#include "common_tilemapcharacter.h"
+#include "common_enemy.h"
+
+constexpr const float speed = 25;
+constexpr const float speedAttack = 100; 
+
+constexpr const float overshotEndCharge = 10;
+constexpr const float overshotReturnSpeed = 50;
+
+constexpr const float hitTime = 0.5f;
+constexpr const float prepareAttackTime = 0.5f;
+constexpr const float exitAttackTime = 0.2f;
+
+constexpr const float spriteScale = .75f;
+constexpr const float spriteRadius = 15.f;
+
+// Area that if intersects with the player will trigger an attack
+constexpr const vec playerNearbyArea = vec(100, 100);
+
+extern float mainClock;
+
+FlyingAlien::FlyingAlien(const vec& pos) 
+	: CircleEntity(pos - vec(0,8), spriteRadius)
+	, state(State::FLYING)
+	, anim(AnimLib::FLYING_ALIEN)
+{
+	orig = this->pos;
+	vel.x = Rand::OnceEach(2)? -speed : speed;
+	screen = ScreenManager::instance()->FindScreenContaining(pos);
+}
+
+Bounds FlyingAlien::ChargeBounds() const
+{
+	Bounds chargeBounds = Bounds(pos, playerNearbyArea);
+	if (vel.x < 0) {
+		chargeBounds.left -= chargeBounds.width;
+		chargeBounds.left += spriteRadius;
+	}
+	else {
+		chargeBounds.left -= spriteRadius;
+	}
+	return chargeBounds;
+}
+
+void FlyingAlien::Update(float dt)
+{
+	if (!InSameScreenAsPlayer(screen)) {
+		return;
+	}
+
+	hitTimer -= dt;
+
+	anim.Update(dt);
+
+	JumpMan* player = JumpMan::instance();
+
+	float walkDir = vel.x > 0 ? 1 : -1;
+
+	switch (state)
+	{
+	case State::CHARGING:
+	{
+		pos.x += vel.x * dt;
+		pos.y = orig.y + walkDir*std::sin(((pos.x - beginAttackX) / playerNearbyArea.x) * M_PI) * playerNearbyArea.y;
+		if (walkDir*(pos.x - beginAttackX) > playerNearbyArea.x) {
+			timer = 0.f;
+			state = State::EXIT_CHARGE;
+		}
+	}
+	break;
+	case State::EXIT_CHARGE:
+	{
+		timer += dt;
+		pos.x += Mates::Lerp(speedAttack, speed, (timer / exitAttackTime)) * walkDir * dt;
+		pos.y = orig.y - std::sin((timer/exitAttackTime) * M_PI/2) * overshotEndCharge;
+		if (timer >= exitAttackTime) {
+			state = State::FLYING;
+			if (pos.x < player->pos.x) {
+				vel.x = speed;
+			} 
+			else {
+				vel.x = -speed;
+			}
+		}
+	}
+	break;
+	case State::ENTER_CHARGE:
+	{
+		timer += dt;
+		if (timer > prepareAttackTime) {
+			state = State::CHARGING;
+			beginAttackX = pos.x;
+			timer = 0.f;
+		}
+	}
+	break;
+	case State::FLYING: 
+	{
+
+		Bounds chargeBounds = ChargeBounds();
+
+		if (IsGoingToHitAWall(chargeBounds.Center(), chargeBounds.Size(), vel, dt)
+			|| IsGoingToLeaveTheScreen(chargeBounds.Center(), chargeBounds.Size(), vel, dt, screen)
+		) {
+			vel.x = -vel.x;
+		}
+		pos.x += vel.x * dt;
+
+		if (Mates::IsNearlyEqual(pos.y, orig.y, 0.5f)) {
+			if (Collide(player->bounds(), ChargeBounds())) {
+				state = State::ENTER_CHARGE;
+				timer = 0.f;
+
+				if (pos.x < player->pos.x) {
+					vel.x = speedAttack;
+				}
+				else {
+					vel.x = -speedAttack;
+				}
+
+				float bigDtAttack = playerNearbyArea.y / speedAttack; // total time that will be spent attacking
+				if (IsGoingToHitAWall(chargeBounds.Center(), chargeBounds.Size(), vel, bigDtAttack)
+					|| IsGoingToLeaveTheScreen(chargeBounds.Center(), chargeBounds.Size(), vel, bigDtAttack, screen))
+				{
+					Debug::out << "Flipping because would hit a wall during the attack";
+					vel.x = -vel.x;
+				}
+			}
+		} else if (pos.y > orig.y) {
+			pos.y -= overshotReturnSpeed*dt;
+		} else if (pos.y < orig.y) {
+			pos.y += overshotReturnSpeed*dt;
+		}
+	}
+	break;
+	}
+
+	Bullet* b = ReceiveDamageFromBullets(bounds());
+	if (b) {
+		hitTimer = hitTime;
+
+		if (state == State::FLYING && IsMovingTowardsInX(pos, vel, b->pos)) {
+			vel.x = -vel.x;
+		}
+	}
+
+	DamagePlayerOnCollision(bounds());
+}
+
+
+void FlyingAlien::Draw() const
+{
+	pos.Debuggerino();
+	vec(pos.x, orig.y).Debuggerino(0, 0, 255);
+
+	if (hitTimer > 0.f) {
+		Assets::tintShader.Activate();
+		Assets::tintShader.SetUniform("flashColor", 1.f, 0.f, 0.f, 0.7f);
+	}
+
+	vec drawPos = pos + vec(vel.x > 0? 2 : -2,(std::sin((orig.x + pos.x)*0.1)*4) - 3);
+	if (state == State::ENTER_CHARGE) {
+		drawPos.y -= sinf((timer / prepareAttackTime) * M_PI) * Tile::size;
+	}
+
+	GPU_Rect rect = anim.GetCurrentRect();
+	Window::Draw(Assets::scifiTexture, drawPos)
+		.withRect(rect)
+		.withScale(vel.x> 0? -spriteScale : spriteScale, spriteScale)
+		.withOrigin(rect.w / 2, rect.h / 2);
+
+	Shader::Deactivate();
+
+	if (Debug::Draw) {
+		bounds().Draw();
+		ChargeBounds().Draw(255, 0, 255);
+	}
+
+}

commit ddf829ae82db784a5378ef6c000501c3bae2ad76

Fix mantis not bouncing properly against each other
Broken in 38d93eb57ce87579cb1a1fb84e6ae75415b52f03

src/mantis.cpp
@@ -54,7 +54,7 @@ bool Mantis::IsBouncingAgainstAnotherMantis() {
 	if (!collidingWith) {
 		return false;
 	}
-	return IsMovingTowardsInY(pos, vel, collidingWith->pos);
+	return IsMovingTowardsInX(pos, vel, collidingWith->pos);
 }
 
 void Mantis::Update(float dt)