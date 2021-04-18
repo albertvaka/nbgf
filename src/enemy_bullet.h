c50a1529346f8ad9eb511a13c433b9c9bec5e345
albertnez <albertmg7@gmail.com>
2021-04-18 09:25:05 +0200

    Tighter enemy bullets.

--- src/enemy_bullet.h
+++ src/enemy_bullet.h
@@ -12,7 +12,7 @@ struct EnemyBullet : CircleEntity, SelfRegister<EnemyBullet>
 	vec vel;
 
 	EnemyBullet(const vec& position, const vec& velocity)
-		: CircleEntity(pos, 5)
+		: CircleEntity(pos, 4)
 	{
 		pos = position;
 		vel = velocity;
