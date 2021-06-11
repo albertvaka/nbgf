62b171439cc2334a8c4c5f074fba4d23e0fb9435
Roger <rogernogueb@gmail.com>
2021-06-11 23:53:57 +0200

    first input test

--- src/player.cpp
+++ src/player.cpp
@@ -7,19 +7,20 @@
 #include "camera.h"
 #include "window.h"
 
-
-const float bulletSpeed = 200.f;
+constexpr uint8_t PlayerSpeed = 500;
 
 Player::Player(float angle, float distance)
 	: distance(distance)
 {
 }
 
-void Player::Update(float dt)
+void Player::Update(float aDt)
 {
-	vec mouseFromCenter = Mouse::GetPositionInWorld() - Camera::Center();
-	pos = Camera::Center() + mouseFromCenter.Normalized() * distance;
-	
+	pos.x -= Input::IsPressed(0, GameKeys::LEFT) * PlayerSpeed * aDt;
+	pos.x += Input::IsPressed(0, GameKeys::RIGHT) * PlayerSpeed * aDt;
+	pos.y += Input::IsPressed(0, GameKeys::DOWN) * PlayerSpeed * aDt;
+	pos.y -= Input::IsPressed(0, GameKeys::UP) * PlayerSpeed * aDt;
+
 }
 
 void Player::Draw() const
