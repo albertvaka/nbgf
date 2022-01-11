612fc1f3a89d8f559d826f8972c8cf8b4add08b3
Albert Vaca Cintora <albertvaka@gmail.com>
2022-01-11 23:00:52 +0100

    Fix KeepDegsBetween0and360

--- engine/angles.h
+++ engine/angles.h
@@ -17,20 +17,20 @@ namespace Angles
 		return (rads * 360.0f) / Tau;
 	}
 
-	inline constexpr void KeepDegsBetween0and360(float& degs)
+	inline constexpr void KeepDegsBetween0and360(float& degs) // [0, 360)
 	{
-		if (degs > 360.f) {
+		if (degs >= 360.f) {
 			degs = fmod(degs, 360.f);
-		} else {
+		} else if (degs < 0.f) {
 			degs = 360.f - fmod(-degs, 360.f);
 		}
 	}
 
-	inline constexpr void KeepRadsBetween0andTau(float& rads)
+	inline constexpr void KeepRadsBetween0andTau(float& rads) // [0, Tau)
 	{
-		if (rads > Tau) {
+		if (rads >= Tau) {
 			rads = fmod(rads, Tau);
-		} else {
+		} else if (rads < 0.f) {
 			rads = Tau - fmod(-rads, Tau);
 		}
 	}
