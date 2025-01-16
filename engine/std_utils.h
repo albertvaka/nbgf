83a49a8111b455485bef6e2c7e02f0b9c1a529cb
Albert Vaca Cintora <albertvaka@gmail.com>
2025-01-16 16:51:17 +0100

    Utility to remove faster from vector when order doesn't matter

---
+++ engine/std_utils.h
@@ -0,0 +1,24 @@
+#include <vector>
+
+/**
+ * Removes elements from an std::vector by swapping the element to remove with
+ * the last element and then popping back, to void paying the cost of shifting
+ * all the elements back one position. Use this if the order isn't important.
+ */
+template <typename T>
+void removeBySwapAndShrink(std::vector<T>& vec, typename std::vector<T>::iterator it) {
+    std::iter_swap(it, vec.end() - 1);
+    vec.pop_back();
+}
+
+/*
+ * Removes elements from an std::vector by swapping the element to remove with
+ * the last element and then popping back, to void paying the cost of shifting
+ * all the elements back one position. Use this if the order isn't important.
+ */
+template <typename T>
+void removeBySwapAndShrink(std::vector<T>& vec, const T& val) {
+    auto it = std::find(vec.begin(), vec.end(), val);
+    std::iter_swap(it, vec.end() - 1);
+    vec.pop_back();
+}
