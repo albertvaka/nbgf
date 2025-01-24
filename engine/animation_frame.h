f3a972bca705da0d724ffad03985fc7ab3973000
Albert Vaca Cintora <albertvaka@gmail.com>
2025-01-24 11:46:59 +0100

    C++20!

--- engine/animation_frame.h
+++ engine/animation_frame.h
@@ -13,14 +13,13 @@ struct AnimationFrame
 	constexpr vec GetSize() const { return vec(rect.w, rect.h); }
 };
 
-// TODO: All constexpr here should be consteval in C++20
 struct SheetFrameCalculator {
 	const int sprite_w;
 	const int sprite_h;
 	const int columns;
 	const int offset_x;
 	const int offset_y;
-	constexpr SheetFrameCalculator(veci size, int columns, veci offset = veci(0,0))
+	consteval SheetFrameCalculator(veci size, int columns, veci offset = veci(0,0))
 		: sprite_w(size.x)
 		, sprite_h(size.y)
 		, columns(columns)
@@ -28,19 +27,19 @@ struct SheetFrameCalculator {
 		, offset_y(offset.y)
 	{}
 	template<int N>
-	constexpr const std::array<AnimationFrame, N> Frames(int begin, float duration) const
+	consteval std::array<AnimationFrame, N> Frames(int begin, float duration) const
 	{
 		return make_array(begin, duration, std::make_index_sequence<N>{});
 	}
-	constexpr const GPU_Rect Rect(int index) const {
+	consteval GPU_Rect Rect(int index) const {
 		return GPU_Rect { (float) (offset_x + sprite_w * (index % columns)), (float) (offset_y + sprite_h * (index / columns)), (float)sprite_w, (float)sprite_h};
 	}
-	constexpr const AnimationFrame Frame(int index, float duration) const {
+	consteval AnimationFrame Frame(int index, float duration) const {
 		return AnimationFrame { Rect(index), duration};
 	}
 private:
 	template<std::size_t... indices>
-	constexpr const std::array<AnimationFrame, sizeof...(indices)> make_array(int begin, float duration, std::index_sequence<indices...>) const
+	consteval std::array<AnimationFrame, sizeof...(indices)> make_array(int begin, float duration, std::index_sequence<indices...>) const
 	{
 		return {{ Frame(begin+indices, duration)... }};
 	}
