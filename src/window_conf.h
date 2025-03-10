9c8b9cfa470d3e1599757a3b6adde13f6f55cb68
Albert Vaca Cintora <albertvaka@gmail.com>
2025-03-10 23:00:51 +0100

    Ship trail

--- src/window_conf.h
+++ src/window_conf.h
@@ -6,5 +6,5 @@ namespace Window
 
 	// Resolution in virtual pixels (gets scaled preserving aspect ratio)
 	constexpr int GAME_HEIGHT = 800;
-	constexpr int GAME_WIDTH = 800;
+	constexpr int GAME_WIDTH = 800*1.666;
 }
