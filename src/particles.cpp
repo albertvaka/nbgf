4e71b5f1c8127c04f852532c5e8a6a1997679c6a
Albert Vaca Cintora <albertvaka@gmail.com>
2025-03-23 15:29:59 +0100

    Particles

---
+++ src/particles.cpp
@@ -0,0 +1,50 @@
+#include "particles.h"
+#include "assets.h"
+#include "anim_lib.h"
+
+namespace Particles {
+
+void InitWaterTrail(PartSys& ps)
+{
+	ps.SetTexture(Assets::backgroundTexture);
+	ps.AddSprite({ 0,0,15,15});
+
+	ps.min_interval = 0.02f;
+	ps.max_interval = 0.05f;
+
+	ps.min_ttl = 1.f;
+	ps.max_ttl = 3.f;
+
+	ps.alpha = 0.7f;
+
+	ps.min_vel.x = -50.f;
+	ps.max_vel.x = 50.f;
+
+	ps.min_vel.y = -50.f;
+	ps.max_vel.y = 50.f;
+
+	ps.min_scale = 1.0f;
+	ps.max_scale = 1.5f;
+
+	ps.scale_vel = -0.8f;
+}
+
+void Init()
+{
+	if (waterTrail.texture == nullptr)
+	{
+		InitWaterTrail(waterTrail);
+	}
+}
+
+void UpdateAll(float dt)
+{
+	waterTrail.UpdateParticles(dt);
+}
+
+void ClearAll()
+{
+	waterTrail.Clear();
+}
+
+}
