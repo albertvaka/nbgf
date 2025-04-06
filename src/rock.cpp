a2120480221d1d89099de6727eb9878c19915a9e
Albert Vaca Cintora <albertvaka@gmail.com>
2025-04-06 12:36:51 +0200

    Consistent placement of rocks

--- src/rock.cpp
+++ src/rock.cpp
@@ -1,12 +1,31 @@
 #include "rock.h"
 
 const float rockSpacing = 15.0f;
-const float rockProbability = 0.00000000001f;
+const float rockProbability = 0.000001f;
 const float chunkSize = 1500.0f;
+
+const GPU_Rect ROCKS_FOAM_RECTS[] = {
+    { 36, 82, 331, 260 },
+    { 396, 54, 411, 292 },
+    { 870, 68, 280, 277 },
+    { 25, 416, 348, 362 },
+    { 432, 423, 334, 356 },
+    { 869, 430, 280, 325 },
+    { 834, 866, 357, 297 },
+    { 446, 845, 334, 341 },
+    { 44, 857, 324, 300 },
+};
+const int ROCKS_FOAM_RECTS_COUNT = 9;
 
 float hash(float x, float y)
 {
-    return std::fmod(std::sin(x * 12.9898f + y * 78.233f) * 43758.5453f, 1.0f);
+    return abs(std::fmod(std::sin(x * 12.9898f + y * 78.233f) * 43758.5453f, 1.0f));
+}
+
+Rock::Rock(const vec& position)
+    : CircleEntity(position, rockRadius)
+    , sprite(ROCKS_FOAM_RECTS[int(hash(position.y, position.x)*ROCKS_FOAM_RECTS_COUNT)])
+{
 }
 
 bool CanSpawn(float x, float y)
@@ -48,7 +67,7 @@ void Rock::SpawnInChunk(int chunkX, int chunkY)
     float bottom = top + chunkSize;
     for (float x = left; x < right; x += rockSpacing) {
         for (float y = top; y < bottom; y += rockSpacing) {
-            if (abs(hash(x, y)) < rockProbability && CanSpawn(x, y)) {
+            if (hash(x, y) < rockProbability && CanSpawn(x, y)) {
                 new Rock(vec(x, y));
             }
         }
