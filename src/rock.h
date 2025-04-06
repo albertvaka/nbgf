acf2eb451ad65b380879e0f2b83bd170d25011f4
Albert Vaca Cintora <albertvaka@gmail.com>
2025-04-06 12:36:51 +0200

    Consistent placement of rocks

--- src/rock.h
+++ src/rock.h
@@ -8,29 +8,12 @@
 
 extern float mainClock;
 
-const GPU_Rect ROCKS_FOAM_RECTS[] = {
-    { 36, 82, 331, 260 },
-    { 396, 54, 411, 292 },
-    { 870, 68, 280, 277 },
-    { 25, 416, 348, 362 },
-    { 432, 423, 334, 356 },
-    { 869, 430, 280, 325 },
-    { 834, 866, 357, 297 },
-    { 446, 845, 334, 341 },
-    { 44, 857, 324, 300 },
-};
-const int ROCKS_FOAM_RECTS_COUNT = 9;
-
 const int rockRadius = 30;
 
 struct Rock : CircleEntity, SelfRegister<Rock>
 {
 	GPU_Rect sprite;
-	Rock(const vec& position)
-		: CircleEntity(position, rockRadius)
-	{
-		sprite = ROCKS_FOAM_RECTS[Rand::roll(ROCKS_FOAM_RECTS_COUNT)];
-	}
+	Rock(const vec& position);
 
 	void Draw() const
 	{
