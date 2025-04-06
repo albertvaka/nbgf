9c16804e8efeed52ee5622322e4b03d8afb140dd
Albert Vaca Cintora <albertvaka@gmail.com>
2025-04-06 19:01:44 +0200

    Smooth zoom

--- src/scene_main.cpp
+++ src/scene_main.cpp
@@ -25,7 +25,6 @@ void SceneMain::EnterScene()
 	immunityTimer = 0.f;
 	Debug::DebugDrawScale = 3.f;
 	ship.Reset();
-	Camera::SetZoom(0.7);
 	for (int i = -1; i <= 1; i++) {
 		for (int j = -1; j <= 1; j++) {
 			Rock::SpawnInChunk(i, j);
